c806b1c34c34de9cc0864abd7eb8c7a80824542d
Leon Molina <leon.maguiar@gmail.com>
2022-05-06 17:47:10 -0300

    feat: setup monorepo

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "395ebb91f64a26",
+    pass: "a275989f62f1d6",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Leon Molina <leon.maguiar@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
